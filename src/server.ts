dd3e7750bf50e166792a190add1748be4b3a4d76
Nikita Yunoshev <nikita.yunoshev@sigma.software>
2020-04-27 02:21:24 +0300

    Prepare for Heroku.

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,16 @@ import { GraphQLServer } from 'graphql-yoga'
 import { permissions } from './permissions'
 import { schema } from './schema'
 import { createContext } from './context'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 new GraphQLServer({
         schema,
         context: createContext,
         middlewares: [permissions],
-}).start(() =>
+}).start( () =>
     console.log(
-        `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
+        `🚀 Server ready at: http://localhost:${process.env.PORT}\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
     ),
 );
