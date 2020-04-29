c094aada0f774bf02e13ee437e2d1a315c8d9169
Nikita Yunoshev <nikita.yunoshev@sigma.software>
2020-04-29 20:50:30 +0300

    Prepare for Heroku.

--- src/types/Query.ts
+++ src/types/Query.ts
@@ -3,41 +3,41 @@ import { getUserId } from '../utils'
 
 export const Query = queryType({
     definition(t) {
-        // t.field('profile', {
-        //     type: 'User',
-        //     nullable: true,
-        //     resolve: (parent, args, ctx) => {
-        //         const userId = getUserId(ctx);
-        //
-        //         return ctx.prisma.user.findOne({
-        //             where: {
-        //                 id: Number(userId),
-        //             },
-        //         })
-        //     },
-        // });
-        //
-        // t.list.field('myPosts', {
-        //     type: 'Post',
-        //     args: {
-        //         userId: intArg({ nullable: false }),
-        //     },
-        //     resolve: (parent, { userId }, ctx) => {
-        //         return ctx.prisma.post.findMany({
-        //             where: { user_id: userId },
-        //         })
-        //     },
-        // });
-        //
-        // t.list.field('publicPosts', {
-        //     type: 'Post',
-        //     resolve: (parent, args, ctx) => {
-        //         return ctx.prisma.post.findMany({
-        //             where: { public: true },
-        //         })
-        //     },
-        // });
+        t.field('profile', {
+            type: 'User',
+            nullable: true,
+            resolve: (parent, args, ctx) => {
+                const userId = getUserId(ctx);
 
-        // t.crud.categories();
+                return ctx.prisma.user.findOne({
+                    where: {
+                        id: Number(userId),
+                    },
+                })
+            },
+        });
+
+        t.list.field('myPosts', {
+            type: 'Post',
+            args: {
+                userId: intArg({ nullable: false }),
+            },
+            resolve: (parent, { userId }, ctx) => {
+                return ctx.prisma.post.findMany({
+                    where: { user_id: userId },
+                })
+            },
+        });
+
+        t.list.field('publicPosts', {
+            type: 'Post',
+            resolve: (parent, args, ctx) => {
+                return ctx.prisma.post.findMany({
+                    where: { public: true },
+                })
+            },
+        });
+
+        t.crud.categories();
     },
 });
