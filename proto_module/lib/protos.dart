92ff030cd18a4322d4bbedfd4ba83b7fa2431afd
Bohdan Martynov <bohdan.martynov@globallogic.com>
2020-06-28 11:35:39 +0300

    edit imports

--- proto_module/lib/protos.dart
+++ proto_module/lib/protos.dart
@@ -1,1 +1,4 @@
 export 'package:proto_module/src/generated/auth/auth.pbgrpc.dart';
+export 'package:proto_module/src/generated/auth/auth.pbenum.dart';
+export 'package:proto_module/src/generated/auth/auth.pb.dart';
+export 'package:proto_module/src/generated/auth/auth.pbjson.dart';
